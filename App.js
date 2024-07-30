c2b3047212841930f809e9bc8853bf61a3308460
emanuelefavero <emafaux89@gmail.com>
2024-07-30 02:35:30 +0200

    add button

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Button } from 'react-native'
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text style={[styles.text, styles.title]}>Hello</Text>
       <Text style={styles.text}>World</Text>
+      <Button
+        title='Press me'
+        onPress={() => alert('Button Pressed!')}
+        color='#a855f7'
+        accessibilityLabel='Press this button to show an alert'
+      />
+
       <StatusBar style='light' backgroundColor='#070923' />
     </View>
   )
