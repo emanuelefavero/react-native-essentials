bac3303bca6c28d8c06df8f7b51857caab928c48
emanuelefavero <emafaux89@gmail.com>
2024-08-23 14:49:58 +0200

    move clear all button to separate component

---
+++ components/ClearAllButton.js
@@ -0,0 +1,29 @@
+import colors from '@/styles/colors'
+import fontSizes from '@/styles/fontSizes'
+import { Text, Pressable, StyleSheet } from 'react-native'
+import { useDispatch, useSelector } from 'react-redux'
+import { setShowAreYouSureModal } from '@/features/modal/showAreYouSureModalSlice'
+
+export default function ClearAllButton() {
+  const dispatch = useDispatch()
+  const todos = useSelector((state) => state.todos)
+
+  if (todos.length === 0) return null
+  return (
+    <Pressable
+      onPress={() => dispatch(setShowAreYouSureModal(true))}
+      style={({ pressed }) => pressed && { opacity: 0.5 }}
+    >
+      <Text style={styles.clearButtonText}>Clear All</Text>
+    </Pressable>
+  )
+}
+
+// ---
+
+const styles = StyleSheet.create({
+  clearButtonText: {
+    color: colors.danger,
+    fontSize: fontSizes.button,
+  },
+})
--- components/Title.js
+++ components/Title.js
@@ -1,26 +1,14 @@
 import colors from '@/styles/colors'
 import fontSizes from '@/styles/fontSizes'
-import { View, Text, Pressable, StyleSheet, useColorScheme } from 'react-native'
-import { useDispatch, useSelector } from 'react-redux'
-import { setShowAreYouSureModal } from '@/features/modal/showAreYouSureModalSlice'
+import { View, Text, StyleSheet } from 'react-native'
+import ClearAllButton from '@/components/ClearAllButton'
 
 export default function Title() {
-  const dispatch = useDispatch()
-  const todos = useSelector((state) => state.todos)
-
   return (
     <View style={styles.titleContainer}>
       <Text style={styles.title}>Todo</Text>
 
-      {/* Clear All Button (Opens Modal) */}
-      {todos.length > 0 && (
-        <Pressable
-          onPress={() => dispatch(setShowAreYouSureModal(true))}
-          style={({ pressed }) => pressed && { opacity: 0.5 }}
-        >
-          <Text style={styles.clearButtonText}>Clear All</Text>
-        </Pressable>
-      )}
+      <ClearAllButton />
     </View>
   )
 }
@@ -42,15 +30,5 @@ const styles = StyleSheet.create({
     color: colors.primary,
     fontSize: fontSizes.title,
     fontWeight: 800,
-  },
-
-  clearButton: {
-    padding: 0,
-    margin: 0,
-  },
-
-  clearButtonText: {
-    color: colors.danger,
-    fontSize: fontSizes.button,
   },
 })
