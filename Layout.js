578492df7fd40ea39804d4499e55064302ee0a76
emanuelefavero <emafaux89@gmail.com>
2024-08-23 14:27:31 +0200

    refactor to use the visible prop in modal

--- Layout.js
+++ Layout.js
@@ -2,7 +2,6 @@ import colors from '@/styles/colors'
 import { View, SafeAreaView, useColorScheme, StyleSheet } from 'react-native'
 import { StatusBar } from 'expo-status-bar'
 import useLoadFonts from '@/hooks/useLoadFonts'
-import { useSelector } from 'react-redux'
 import Title from '@/components/Title'
 import Todos from '@/components/Todos'
 import AddTodo from '@/components/AddTodo'
@@ -11,7 +10,6 @@ import AreYouSureModal from '@/components/AreYouSureModal'
 export default function Layout() {
   const colorScheme = useColorScheme() // dark mode
   const { loaded, error } = useLoadFonts() // load fonts
-  const showAreYourSureModal = useSelector((state) => state.showAreYouSureModal)
 
   if (!loaded && !error) return null // show nothing while fonts are loading
 
@@ -35,9 +33,7 @@ export default function Layout() {
         <AddTodo />
         <Title />
         <Todos />
-
-        {/* Are You Sure Modal */}
-        {showAreYourSureModal && <AreYouSureModal />}
+        <AreYouSureModal />
       </View>
     </SafeAreaView>
   )
--- components/AreYouSureModal.js
+++ components/AreYouSureModal.js
@@ -1,8 +1,11 @@
 import { Modal, View, Text, Button } from 'react-native'
+import { useSelector } from 'react-redux'
 
 export default function AreYouSureModal() {
+  const showAreYourSureModal = useSelector((state) => state.showAreYouSureModal)
+
   return (
-    <Modal>
+    <Modal visible={showAreYourSureModal} animationType='slide'>
       <View>
         <Text>Delete all tasks?</Text>
         <Text>This action cannot be undone.</Text>
