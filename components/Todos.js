d9f0ca2a91a76b7632dc28fe99a6eb252e38fd70
emanuelefavero <emafaux89@gmail.com>
2024-08-28 08:39:02 +0200

    add separator between completed and not completed todos

--- components/Todos.js
+++ components/Todos.js
@@ -1,15 +1,53 @@
-import { FlatList, StyleSheet } from 'react-native'
+import colors from '@/styles/colors'
+import React from 'react'
+import { FlatList, View, Text, useColorScheme, StyleSheet } from 'react-native'
 import { useSelector } from 'react-redux'
 import Todo from '@/components/Todo'
 
 export default function Todos() {
+  const colorScheme = useColorScheme()
   const todos = useSelector((state) => state.todos)
+
+  // Separate todos into incomplete and completed
+  const incompleteTodos = todos.filter((todo) => !todo.completed)
+  const completedTodos = todos.filter((todo) => todo.completed)
 
   return (
     <FlatList
-      data={todos}
+      data={incompleteTodos}
       renderItem={({ item: todo }) => <Todo todo={todo} />}
       keyExtractor={(todo) => todo.id}
+      ListFooterComponent={() => (
+        <>
+          {incompleteTodos.length > 0 && completedTodos.length > 0 && (
+            <View style={styles.separator} />
+          )}
+          {completedTodos.length > 0 && (
+            <FlatList
+              data={completedTodos}
+              renderItem={({ item: todo }) => <Todo todo={todo} />}
+              keyExtractor={(todo) => todo.id}
+              ListHeaderComponent={
+                completedTodos.length > 0 ? (
+                  <Text
+                    style={[
+                      styles.header,
+                      {
+                        color:
+                          colorScheme === 'dark'
+                            ? colors.textDark
+                            : colors.text,
+                      },
+                    ]}
+                  >
+                    Completed
+                  </Text>
+                ) : null
+              }
+            />
+          )}
+        </>
+      )}
       style={styles.todosContainer}
       alwaysBounceVertical={false}
     />
@@ -21,5 +59,15 @@ export default function Todos() {
 const styles = StyleSheet.create({
   todosContainer: {
     flex: 1,
+  },
+  header: {
+    fontFamily: 'SF Pro Rounded',
+    fontSize: 26,
+    fontWeight: 'bold',
+    paddingVertical: 12,
+  },
+  separator: {
+    height: 1,
+    backgroundColor: '#ccc',
   },
 })
