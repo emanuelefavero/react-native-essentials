5da672675fbeb05ec2eabf232b9024abea590ecf
emanuelefavero <emafaux89@gmail.com>
2024-08-29 23:44:43 +0200

    add Home Screen styles

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -1,10 +1,34 @@
-import { Button } from 'react-native'
+import colors from '@/styles/colors'
+import { View, Button, StyleSheet, useColorScheme } from 'react-native'
 
 export default function HomeScreen({ navigation }) {
+  const colorScheme = useColorScheme() // dark mode
+
   return (
-    <Button
-      title='Go to TodoList'
-      onPress={() => navigation.navigate('TodoList')}
-    />
+    <View
+      style={[
+        styles.appContainer,
+        colorScheme === 'dark' && darkStyles.appContainer,
+      ]}
+    >
+      <Button
+        title='Go to TodoList'
+        onPress={() => navigation.navigate('TodoList')}
+      />
+    </View>
   )
 }
+
+const styles = StyleSheet.create({
+  appContainer: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: colors.background,
+  },
+})
+
+const darkStyles = StyleSheet.create({
+  appContainer: {
+    backgroundColor: colors.backgroundDark,
+  },
+})
