e11158b41b19c337b8ed827a5b7dfedc1a632c60
emanuelefavero <emafaux89@gmail.com>
2024-09-07 13:44:16 +0200

    add - increase opacity to red swipe background color

--- styles/colors.js
+++ styles/colors.js
@@ -1,6 +1,6 @@
 const primary = '#22c55e'
 const danger = '#f43f5e'
-const dangerMuted = 'rgba(244, 63, 94, 0.17)'
+const dangerMuted = 'rgba(244, 63, 94, 0.25)'
 const button = '#007aff' // iOS button color
 
 // Text
