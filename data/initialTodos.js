f32e4f5c0c5ee13d5f059b784a8782d874567499
emanuelefavero <emafaux89@gmail.com>
2024-09-09 09:21:51 +0200

    add - change initial todos

--- data/initialTodos.js
+++ data/initialTodos.js
@@ -1,8 +1,13 @@
 import uuid from 'react-native-uuid'
 
 export const initialTodos = [
-  { id: uuid.v4(), value: 'Add a new todo', completed: false },
-  { id: uuid.v4(), value: 'Click a todo to complete it', completed: false },
+  { id: uuid.v4(), value: 'Add a new task', completed: false },
+  { id: uuid.v4(), value: 'Click a task to complete it', completed: false },
   { id: uuid.v4(), value: '<- Or swipe left to complete it', completed: false },
-  { id: uuid.v4(), value: '-> Swipe right to delete a todo', completed: true },
+  { id: uuid.v4(), value: '-> Swipe right to delete a task', completed: false },
+  {
+    id: uuid.v4(),
+    value: 'Press and hold a task to edit it.',
+    completed: true,
+  },
 ]
