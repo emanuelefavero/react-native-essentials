09dd1f6eb5016fbc1d3fab86ce7f2ce19724bc29
emanuelefavero <emafaux89@gmail.com>
2024-09-09 02:54:26 +0200

    udpate initial todos

--- data/initialTodos.js
+++ data/initialTodos.js
@@ -1,7 +1,8 @@
 import uuid from 'react-native-uuid'
 
 export const initialTodos = [
-  { id: uuid.v4(), value: 'Work', completed: false },
-  { id: uuid.v4(), value: 'Eat', completed: false },
-  { id: uuid.v4(), value: 'Sleep', completed: true },
+  { id: uuid.v4(), value: 'Add a new todo', completed: false },
+  { id: uuid.v4(), value: 'Click a todo to complete it', completed: false },
+  { id: uuid.v4(), value: '<- Or swipe left to complete it', completed: false },
+  { id: uuid.v4(), value: '-> Swipe right to delete a todo', completed: true },
 ]
